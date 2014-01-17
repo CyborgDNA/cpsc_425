q4 done. fix: don't forget about normalization on q1

assignment_2/q2.py
@@ -5,10 +5,11 @@ from scipy import signal
 
 
 def gauss1d(sigma):
-    assert sigma != 0, "Sigma must be non-zero"
+    assert sigma > 0, "Sigma greather than zero"
     factor = round(6*sigma)
     array_length = factor if factor % 2 else factor+1  # the filter must have a odd center
 
     # exp(- x^2 / (2*sigma^2))
     x = np.arange((-array_length+1)/2, (array_length+1)/2, 1)  # generates the distance from center array
-    return np.exp(-np.square(x)/(2*sigma**2))  # calculates the gaussian function to each element
+    x = np.exp(-np.square(x)/(2*sigma**2))  # calculates the gaussian function to each element
+    return x/sum(x)  # return the normalized array