e634e13598452c48947ad92361375ce90ae47c2c
rafadaguiar <rfna@cin.ufpe.br>
2014-01-18 14:41:37 -0800

    add comments on q4

--- assignment_2/q4.py
+++ assignment_2/q4.py
@@ -8,13 +8,13 @@ IMG_PATH = 'data/'
 
 def im_prep(image):
     im = Image.open(IMG_PATH+image)
-    im = im.convert('L')
-    im.save(IMG_PATH+"out_img1.png","PNG")
-    return np.asarray(im)
+    im = im.convert('L')  # converts the img to bw
+    im.save(IMG_PATH+"out_img1.png","PNG")  # saves the bw img to further comparison
+    return np.asarray(im)  # returns the array representation
 
 
 def gaussconvolve2d(image, sigma):
-    im = signal.convolve2d(im_prep(image), gauss2d(sigma), 'same')
-    im = Image.fromarray(im)
+    im = signal.convolve2d(im_prep(image), gauss2d(sigma), 'same')  # applies the gaussian blur on the array
+    im = Image.fromarray(im)  # converts it back to image representation
     im = im.convert("RGB")
     im.save(IMG_PATH+"out_img2.png","PNG")
